bc02e50e654e1be8e525bb0ce9229fbef9fc9b09
ae2f <mincraft20046@gmail.com>
2025-03-11 20:35:50 +0000

    # Shell > Added conditonal logic for cross-platform. > Running it with git bash would hopefully make a project for Windows. (which seems not to be really needed)
    # UEFlags
    - Additional flags if you want to

--- init.sh
+++ init.sh
@@ -1,4 +1,38 @@
+# Basics
 PWD=$(pwd)
-UERoot=~/UE2
 
-sh $UERoot/GenerateProjectFiles.sh $PWD/TCG_Sample.uproject -game
+# Parameters
+# You may change them
+UERoot=~/UE2 
+UEFlags=""
+
+
+# Variables
+osname=$(uname -s)
+proj=$PWD/TCG_Sample.uproject
+projgen=$UERoot/GenerateProjectFiles
+
+shell=""
+shell_ext=""
+
+case "$osname" in
+	Linux)
+		shell=sh
+		shell_ext=sh
+
+		;;
+	Darwin)
+		shell=sh
+		shell_ext=sh
+		;;
+	MINGW*|MSYS*)
+		shell=cmd
+		shell_ext=bat
+		;;
+
+	*)
+		echo "I have no clue what are you running this on"
+		;;
+esac
+
+$shell $projgen.$shell_ext $proj -game # $UEFlags
